299d187a57a14c0e3bd253e030085ab2a8931947
Lav <lav@lav-mac.local>
2022-10-12 21:26:17 +0530

    Create and Destroy session

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import logo from "./logo.svg";
 import "./App.css";
 import Web3 from "web3";
 import { ethers } from "ethers";
-import LoadWeb3 from "./web3";
+import {
+  CreateUserSession,
+  DestroyUserSession,
+  GetAccounts,
+  GetInfuraId,
+  LoadWeb3,
+} from "./web3";
 
 function App() {
   return (
     <div className="App">
       <button className="button" onClick={() => LoadWeb3()}>
-        Click me
+        Connect
+      </button>
+      <button className="button" onClick={() => GetAccounts()}>
+        Get address
+      </button>
+      <button className="button" onClick={() => GetInfuraId()}>
+        Infura Id
+      </button>
+      <button className="button" onClick={() => CreateUserSession()}>
+        Create Session Api
+      </button>
+      <button className="button" onClick={() => DestroyUserSession()}>
+        Destroy Session Api
       </button>
     </div>
   );
--- src/web3.js
+++ src/web3.js
@@ -1,34 +1,26 @@
 import Web3 from "web3";
+// import axios from "axios";
 import { ethers } from "ethers";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
-const rpc = process.env.ethereum_provider;
+const rpc = "https://goerli.infura.io/v3/3e19c57faceb42c5a67bd39c08258898";
 
-// const LoadWeb3 = async () => {
-//   let web3 = await new Web3(window.ethereum);
-//   if ((window.ethereum == "metamask") ||  typeof window.wallet == "undefined") {
-//     let provider = await new ethers.providers.Web3Provider(window.ethereum);
-//     let signer = await provider.getSigner();
-//     await window.ethereum.request({ method: "eth_requestAccounts" });
-//     console.log("Signer", await signer.getAddress());
-//     return await signer.getAddress();
-//   } else {
-//     alert("Please install Metamask");
-//   }
-// };
+let signer;
+let provider;
+let tprovider;
+let chainId = process.env.CHAIN_ID;
 
 async function LoadWeb3() {
-  let signer;
-  let provider;
-  let tprovider;
   window.web3 = new Web3(window.ethereum);
   console.log("-----------------------");
-  console.log(window.ethereum);
+  console.log(window.wallet);
   console.log("-----------------------");
   if (
-    (window.ethereum  == "metamask") ||
+    (window.ethereum && window.wallet == "metamask") ||
     typeof window.wallet == "undefined"
   ) {
+    /** Defining the provider in the function and can be withdrawl outside
+     * the function */
     provider = new ethers.providers.Web3Provider(window.ethereum);
     signer = provider.getSigner();
     await window.ethereum.request({ method: "eth_requestAccounts" });
@@ -51,4 +43,80 @@ async function LoadWeb3() {
   //   window.signer = provider.getSigner();
   // }
 }
-export default LoadWeb3;
+
+const GetAccounts = async () => {
+  try {
+    if (window.wallet == "walletConnect") {
+      const signer = window.provider.getSigner();
+      var accounts = await signer.getAddress();
+    } else {
+      const signer = provider.getSigner();
+      var accounts = await signer.getAddress();
+    }
+    console.log("--------->", accounts);
+    return accounts;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const GetInfuraId = async () => {
+  const url = new URL(rpc);
+  const path = url.pathname.split("/");
+  console.log("Infura Id", path[path.length - 1] ?? "");
+  return path[path.length - 1] ?? "";
+};
+
+const CreateUserSession = async (
+  address,
+  balance,
+  destroySession,
+  wallet = window.wallet
+) => {
+  const config = {
+    method: "POST",
+    headers: {
+      "X-CSRF-TOKEN": 'meta[name="csrf-token"]'.attr("content"), // showing error
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: {
+      address: address,
+      balance: balance,
+      destroy_session: destroySession,
+      wallet,
+    },
+  };
+  const resp = await fetch(`http://localhost:3001/session`, config)
+    .then((response) => {
+      console.log("fadsf");
+      return resp;
+    })
+    .catch((err) => {
+      console.log("User Session Create Error", err);
+    });
+  return resp;
+};
+
+const DestroyUserSession = async (address) => {
+  const config = {
+    method: "DELETE",
+    data: {},
+    headers: {
+      "X-CSRF-TOKEN": '[name="csrf-token"]'[0].content,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+  const resp = fetch(`/sessions/${address}`, config)
+    .then((response) => response)
+    .catch((e) => console.log("Session Error", e));
+  return resp;
+};
+export {
+  LoadWeb3,
+  GetAccounts,
+  GetInfuraId,
+  CreateUserSession,
+  DestroyUserSession,
+};
